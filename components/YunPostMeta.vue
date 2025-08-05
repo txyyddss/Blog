6050911bf08b90240e8e17aa1805c885dbed4117
txyyddss <tx@1919801.xyz>
2025-08-05 10:36:46 +0800

    fix

---
+++ components/YunPostMeta.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import type { Post } from 'valaxy'
+import { useLayout } from 'valaxy'
+import YunPostMeta from 'valaxy-theme-yun/components/YunPostMeta.vue'
+import { useAddonVercount } from 'valaxy-addon-vercount'
+
+defineProps<{
+  frontmatter: Post
+}>()
+
+// 仅在 Post 布局显示
+const isPost = useLayout('post')
+const { page } = useAddonVercount()
+</script>
+
+<template>
+  <YunPostMeta :frontmatter="frontmatter">
+    <span v-if="isPost" class="yun-post-stat">
+      <span>阅读数: {{ page.pv }}</span>
+      <span style="margin-left: 0.5em;">访客数: {{ page.uv }}</span>
+    </span>
+  </YunPostMeta>
+</template>
